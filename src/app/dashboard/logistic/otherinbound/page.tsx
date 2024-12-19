6c46618a4c601b14bdcaf53cec0712887418a354
Naufal Abrori <nopal.abrori@gmail.com>
2024-12-19 10:38:38 +0700

    fix: column using usememo

--- src/app/dashboard/logistic/otherinbound/page.tsx
+++ src/app/dashboard/logistic/otherinbound/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useEffect } from "react";
-import { otherInboundHeaderColumns } from "@/components/modules/dashboard/logistic/otherinbound/columns";
 import { OtherInboundHeaderDataTable } from "@/components/modules/dashboard/logistic/otherinbound/data-table";
 import useMenuStore from "@/hooks/useMenuStore";
 
@@ -8,12 +7,12 @@ export default function OtherInboundPage() {
   const { setMenu } = useMenuStore();
 
   useEffect(() => {
-    setMenu("Other Inbound")
+    setMenu("Other Inbound");
   }, [setMenu]);
 
   return (
     <div>
-      <OtherInboundHeaderDataTable columns={otherInboundHeaderColumns} initialData={[]} />
+      <OtherInboundHeaderDataTable />
     </div>
   );
 }
--- src/components/modules/dashboard/logistic/otherinbound/columns.tsx
+++ src/components/modules/dashboard/logistic/otherinbound/columns.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { ColumnDef } from "@tanstack/react-table";
 import { MoreHorizontal } from "lucide-react";
@@ -11,65 +12,81 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Badge } from "@/components/ui/badge";
 import { OtherInboundHeader } from "@/services/OtherInbound/types";
+import { useMemo } from "react";
 
-export const otherInboundHeaderColumns: ColumnDef<OtherInboundHeader>[] = [
-  // {
-  //   id: "numbers",
-  //   header: "No",
-  //   cell: (info) => info.row.index + 1
-  // },
-  {
-    accessorKey: "code",
-    header: () => "Code"
-  },
-  {
-    accessorKey: "businessPartner",
-    header: () => "Business Partner"
-  },
-  {
-    accessorKey: "bpOrder",
-    header: () => "BP Order",
-  },
-  {
-    accessorKey: "remarks",
-    header: () => "Remarks",
-  },
-  {
-    accessorKey: "transactionStatus",
-    header: "Status",
-    cell: ({ row }) => {
-      const status = row.getValue("transactionStatus") as string;
-      return (
-        <Badge
-          className={
-            status === "Closed"
-              ? "bg-red-500 hover:bg-red-600"
-              : "bg-green-500 hover:bg-green-600"
-          }
-        >
-          {status}
-        </Badge>
-      );
-    },
-  },
-  {
-    id: "actions",
-    header: "Action",
-    cell: () => {
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem>View</DropdownMenuItem>
-            <DropdownMenuItem>Delete</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
-];
+interface ColumnOtherInboundHeader {
+  currentPage: number;
+  perPage: number;
+}
+
+export const OtherInboundHeaderColumns = ({
+  currentPage,
+  perPage,
+}: ColumnOtherInboundHeader) => {
+  const columns = useMemo<ColumnDef<any, OtherInboundHeader>[]>(
+    () => [
+      {
+        id: "numbers",
+        header: "No",
+        cell: (info) => perPage * (currentPage - 1) + (info?.row.index + 1),
+      },
+      {
+        accessorKey: "code",
+        header: () => "Code",
+      },
+      {
+        accessorKey: "businessPartner",
+        header: () => "Business Partner",
+      },
+      {
+        accessorKey: "bpOrder",
+        header: () => "BP Order",
+      },
+      {
+        accessorKey: "remarks",
+        header: () => "Remarks",
+      },
+      {
+        accessorKey: "transactionStatus",
+        header: "Status",
+        cell: ({ row }) => {
+          const status = row.getValue("transactionStatus") as string;
+          return (
+            <Badge
+              className={
+                status === "Closed"
+                  ? "bg-red-500 hover:bg-red-600"
+                  : "bg-green-500 hover:bg-green-600"
+              }
+            >
+              {status}
+            </Badge>
+          );
+        },
+      },
+      {
+        id: "actions",
+        header: "Action",
+        cell: () => {
+          return (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                  <span className="sr-only">Open menu</span>
+                  <MoreHorizontal className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>View</DropdownMenuItem>
+                <DropdownMenuItem>Delete</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          );
+        },
+      },
+    ],
+    [currentPage, perPage]
+  );
+
+  return columns;
+};
